c5569a9896909d741781065894bba99a93321190
Pelumi Akinlotan <pelyboyakinlotan@gmail.com>
2025-03-14 00:50:24 +0000

    Repository Update - 2025-03-14 00:50:24

--- ExtraCredit/drinking.py
+++ ExtraCredit/drinking.py
@@ -7,5 +7,24 @@ If the user does not have their ID, display to the user that they cannot drink.
 If the user does not input yes or no, display to the user they cannot drink.
 """
 
-if __name__ == "__main__":
-    ...
+
+    if __name__ == "__main__":
+
+    id_response = input("Do you have your ID? (yes or no): ").strip().lower()
+
+    if id_response == "yes":
+       
+        age_response = input("Are you 21 years or older? (yes or no): ").strip().lower()
+        
+        if age_response == "yes":
+            print("You can drink.")
+        elif age_response == "no":
+            print("You cannot drink.")
+        else:
+            print("You cannot drink.")
+    
+    elif id_response == "no":
+        print("You cannot drink.")
+    
+    else:
+        print("You cannot drink.")

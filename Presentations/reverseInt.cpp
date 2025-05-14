e624654eba81bb8bf685d47acea3966e34131db5
Pelumi Akinlotan <pelyboyakinlotan@gmail.com>
2025-05-14 02:14:18 +0000

    Repository Update - 2025-05-14 02:14:17

--- Presentations/reverseInt.cpp
+++ Presentations/reverseInt.cpp
@@ -38,3 +38,51 @@ void reverseIntTest()
     assert(reverseInt(1000) == 1);
     assert(reverseInt(-1000) == -1);
 }
+
+
+// Pelumi
+
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include "_functions.h"
+
+using namespace std;
+using namespace Presentations;
+
+void reverseIntTest();
+
+// Function to reverse an integer
+int reverseInt(int num)
+{
+    int reversed = 0;
+    int sign = (num < 0) ? -1 : 1; // Store the sign of the number
+    num = abs(num); // Make the number positive for processing
+
+    while (num > 0)
+    {
+        int digit = num % 10;          // Get the last digit
+        reversed = reversed * 10 + digit; // Add digit to reversed number
+        num /= 10;                    // Remove the last digit
+    }
+
+    return reversed * sign; // Return with original sign
+}
+
+int main()
+{
+    reverseIntTest();
+    return 0;
+}
+
+void reverseIntTest()
+{
+    // reverseInt
+    assert(reverseInt(1234) == 4321);
+    assert(reverseInt(-1234) == -4321);
+    assert(reverseInt(0) == 0);
+    assert(reverseInt(1000) == 1);
+    assert(reverseInt(-1000) == -1);
+
+    cout << "All reverseInt tests passed!" << endl;
+}
